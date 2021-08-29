14ba572c3065ea3c05479baedc3db1faa483d8f0
Estarlin R <89669432+estarlincito@users.noreply.github.com>
2021-08-29 16:21:55 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -15,6 +15,51 @@ var tg = document.getElementById("tg");
 var po = [0,1,2,3,4,5,6,7,8,9];
 var b = 0;
 
+
+//sound
+var sound1 = buttonAdd.src = "sound/buttonAdd.wav";
+var sound2 = buttonRemove.src = "sound/buttonRemove.wav";
+var sound3 = clickError.src = "sound/clickError.wav";
+
+//set
+soundLocalStorage = ["sound1","sound2","sound3"]
+soundLocalStorage[0] = localStorage.setItem("sound1", sound1);
+soundLocalStorage[1] = localStorage.setItem("sound2", sound2);
+soundLocalStorage[2] = localStorage.setItem("sound3", sound3);
+
+//get
+getSoundLocalStorage = ["sound1","sound2","sound3"];
+getSoundLocalStorage[0] = localStorage.getItem("sound1");
+getSoundLocalStorage[1] = localStorage.getItem("sound2");
+getSoundLocalStorage[2] = localStorage.getItem("sound3");
+
+//buttonAdd
+function buttonAdd(){
+  var buttonAdd = new Audio();
+  buttonAdd.src = getSoundLocalStorage[0];
+  buttonAdd.play();
+}
+
+//buttonRemove
+function buttonRemove(){
+  var buttonRemove = new Audio();
+      buttonRemove.src = getSoundLocalStorage[1];
+      buttonRemove.play();
+}
+//clickError
+function clickError(){
+
+  var clickError = new Audio();
+  clickError.src = getSoundLocalStorage[2];
+  clickError.play();
+
+  setTimeout(() => {
+    alert(`Deja de tacaño y compra la version PRO 😏`)
+  }, 200);
+
+}
+
+
 //setLocalStorage
 var taskLocalStorage = ["n1", "n2", "n3", "n4", "n5", "n6", "n7", "n8", "n9", "n10"];
 
@@ -36,7 +81,6 @@ var getLocalStorage = ["n1", "n2", "n3", "n4", "n5", "n6", "n7", "n8", "n9", "n1
 function agregar(){
   tituloT = titulo[0].value;
   contenidoT = contenido[0].value;
-
   b++;
   //array task
   var task = [
@@ -140,6 +184,7 @@ function agregar(){
           taskLocalStorage[0] = localStorage.setItem("n1", task[0]);
           tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[0]}</span>`;
           `${titulo[0].value = ""}${contenido[0].value = ""}`
+          buttonAdd();
 
     }else  if (po[1] === 1){
             po[1]++;
@@ -147,62 +192,73 @@ function agregar(){
             taskLocalStorage[1] = localStorage.setItem("n2", task[1]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[1]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
     }else  if (po[2] === 2){
             po[2]++;
             task3.innerHTML = task[2];
             taskLocalStorage[2] = localStorage.setItem("n3", task[2]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[2]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
     }else  if (po[3] === 3){
             po[3]++;
             task4.innerHTML = task[3];
             taskLocalStorage[3] = localStorage.setItem("n4", task[3]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[3]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
     }else  if (po[4] === 4){
             po[4]++;
             task5.innerHTML = task[4];
             taskLocalStorage[4] = localStorage.setItem("n5", task[4]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[4]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
     }else  if (po[5] === 5){
             po[5]++;
             task6.innerHTML = task[5];
             taskLocalStorage[5] = localStorage.setItem("n6", task[5]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[5]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
     }else  if (po[6] === 6){
             po[6]++;
             task7.innerHTML = task[6];
             taskLocalStorage[6] = localStorage.setItem("n7", task[6]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[6]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
     }else  if (po[7] === 7){
             po[7]++;
             task8.innerHTML = task[7];
             taskLocalStorage[7] = localStorage.setItem("n8", task[7]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[7]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
     }else  if (po[8] === 8){
             po[8]++;
             task9.innerHTML = task[8];
             taskLocalStorage[8] = localStorage.setItem("n9", task[8]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[8]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
     }else  if (po[9] === 9){
             po[9]++;
             task10.innerHTML = task[9];
             taskLocalStorage[9] = localStorage.setItem("n10", task[9]);
             tg.innerHTML = `Tarea guardada en <span class ="tgId">ID_${po[9]}</span>`;
             `${titulo[0].value = ""}${contenido[0].value = ""}`
+            buttonAdd();
       }
     }else{
-      alert(`Deja de tacaño y compra la version PRO 😏`)
+     b = 10;
+     clickError();
     }
 }
 
 // quitar
 function quitar(n1){
+  buttonRemove();
   b--;
   switch (n1) {
        case 1:
@@ -278,51 +334,50 @@ function quitar(n1){
        default:
             break;
     }
-
  }
 
  //tareasSaves
  function tareasSaves(){
   if (getLocalStorage[0]){
     task1.innerHTML = getLocalStorage[0];
-    po[0] = 1;
-    b = 1;
+    po[0]++;
+    b++;
   }if (getLocalStorage[1]) {
     task2.innerHTML = getLocalStorage[1];
-    po[1] = 2;
-    b = 2;
+    po[1]++;
+    b++;
   }if (getLocalStorage[2]) {
     task3.innerHTML = getLocalStorage[2];
-    po[2] = 3;
-    b = 3;
+    po[2]++;
+    b++;
   }if (getLocalStorage[3]) {
     task4.innerHTML = getLocalStorage[3];
-    po[3] = 4;
-    b = 4;
+    po[3]++;
+    b++;
   }if (getLocalStorage[4]) {
     task5.innerHTML = getLocalStorage[4];
-    po[4] = 5;
-    b = 5;
+    po[4]++;
+    b++;
   }if (getLocalStorage[5]) {
     task6.innerHTML = getLocalStorage[5];
-    po[5] = 6;
-    b = 6;
+    po[5]++;
+    b++;
   }if (getLocalStorage[6]) {
     task7.innerHTML = getLocalStorage[6];
-    po[6] = 7;
-    b = 7;
+    po[6]++;
+    b++;
   }if (getLocalStorage[7]) {
     task8.innerHTML = getLocalStorage[7];
-    po[7] = 8;
-    b = 8;
+    po[7]++;
+    b++;
   }if (getLocalStorage[8]) {
     task9.innerHTML = getLocalStorage[8];
-    po[8] = 9;
-    b = 9;
+    po[8]++;
+    b++;
   }if (getLocalStorage[9]) {
     task10.innerHTML = getLocalStorage[9];
-    po[9] = 10;
-    b = 10;
+    po[9]++;
+    b++;
   }
  }
 
